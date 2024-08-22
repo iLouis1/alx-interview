36db98c3f241fdd9af80b20285e9ffa5d3779bbf
iLouis1 <rhiganakatugba@gmail.com>
2024-08-22 15:07:44 -0700

    Done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,9 +18,9 @@ def makeChange(coins, total):
     table[0] = 0
     m = len(coins)
     for k in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= k:
-                subres = table[k - coins[j]]
+        for v in range(m):
+            if coins[v] <= k:
+                subres = table[k - coins[v]]
                 if subres != sys.maxsize and subres + 1 < table[k]:
                     table[k] = subres + 1
 
